9c6ce75a85777a34f2c64b594007dd77c8b83fca
David Mandel <mandeldl@gmail.com>
2016-11-26 11:02:37 -0500

    getting back to basics

--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
+// Create new component, produce some html
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
+const App = function() {
+	return <div>Hi!</div>
+}
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
-
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+// Put the component into the DOM: first arg is the React Component (<Component />)
+// 2nd argument is the html element (why they use class instead of id is not clear...)
+ReactDOM.render(<App />, document.querySelector('.container'));
