0dcd035d72a90975b8c24c3671ac33d425edaf0a
David Mandel <mandeldl@gmail.com>
2016-11-26 10:49:30 -0500

    another test

--- src/components/app.js
+++ src/components/app.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export default class App extends Component {
   render() {
     return (
-      <div>Testing one two three</div>
+      <div>Another test</div>
     );
   }
 }
