cffad3dbf0ea229d1177fb56efed686318020468
David Mandel <mandeldl@gmail.com>
2016-11-28 13:01:24 -0500

    add basic input

---
+++ src/components/search_bar.js
@@ -0,0 +1,7 @@
+import React from 'react';  //You always need to import React, even if you're not explicitly calling it.
+
+const SearchBar = () => {
+	return <input />;
+};
+
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -2,10 +2,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+//Components:
+import SearchBar from './components/search_bar';  //Note relative paths, .js extension isn't necessary
+
 import {API_KEY} from './API_KEY';  //hidden because of Gitignore
 
 const App = () => {
-	return <div>Hello world!!</div>
+	return (
+			<div>
+				<SearchBar />  
+			</div>
+			)
+		//You can include components inside others, usually use () for multi-line HTML.
 }
 
 // Put the component into the DOM: first arg is the React Component (<Component />)
