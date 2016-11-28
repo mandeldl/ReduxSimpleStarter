5f3f46187cc42e3e7c6c15be59faf18ee79fc7de
David Mandel <mandeldl@gmail.com>
2016-11-28 13:04:57 -0500

    use class-based syntax for searchbar component

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,7 +1,10 @@
 import React from 'react';  //You always need to import React, even if you're not explicitly calling it.
 
-const SearchBar = () => {
-	return <input />;
-};
+//ES6 class:
+class SearchBar extends React.Component {  //extends functionality of React Component
+	render() { //how you define methods on ES6 classes; all React Components must have render method
+		return <input />;
+	}
+}
 
 export default SearchBar;
