fd3aef6678ba36158d25d25c921e4e7d5d76bfe4
David Mandel <mandeldl@gmail.com>
2016-11-29 15:30:36 -0500

    add markup for videolistitems

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,7 +1,23 @@
 import React from 'react';
 
-const VideoListItem = (props) => {
-	return <li className={props.video.kind}>Video {props.video.id.videoId}</li>
+const VideoListItem = ({video}) => {
+	//using {video} is equivalent to const video = props.video; ES6 destructuring again!!
+
+	const imageUrl = video.snippet.thumbnails.default.url;
+	const title = video.snippet.title;
+
+	return (
+		<li className="list-group-item">
+			<div className="video-list media">
+				<div className="media-left">
+					<img className="media-object" src={imageUrl} />
+					<div className="media-body">
+						<div className="media-heading">{title}</div>
+					</div>
+				</div>
+			</div>
+		</li>
+	);
 };
 
 export default VideoListItem;
