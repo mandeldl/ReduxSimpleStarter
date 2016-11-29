6e6acea8b4c1647963f7f6500e360b787aaa49a3
David Mandel <mandeldl@gmail.com>
2016-11-29 11:42:03 -0500

    implement basic yt api search

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 // Create new component, produce some html
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 //Components:
 import SearchBar from './components/search_bar';  //Note relative paths, .js extension isn't necessary
 
-import {API_KEY} from './API_KEY';  //hidden because of Gitignore
+//Special API Key for YouTube, hidden because of Gitignore
+import {API_KEY} from './API_KEY';  
+
+//YTSearch takes an object that uses the api key and a search term, 2nd param is callback function for data.
+YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
+	console.log(data);
+});
 
 const App = () => {
 	return (
