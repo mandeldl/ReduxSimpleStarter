d884d6e4379e615f475e3ab5ade5f0659f88ef1b
David Mandel <mandeldl@gmail.com>
2016-11-30 14:45:45 -0500

    handle null props

--- src/components/video_detail.js
+++ src/components/video_detail.js
@@ -1,13 +1,19 @@
 import React from 'react';
 
 const VideoDetail = ({video}) => {
+	//handle null props:
+
+	if (!video) {
+		return (<div>Loading...</div>);
+	}
+
 	const videoId = video.id.videoId;
 	const url = `https://www.youtube.com/embed/${videoId}`;  //cool string interpolation from ES6!
 
 	return (
 			<div className="video-detail col-md-8">
 				<div className="embed-responsive embed-responsive-16by9">
-					<iframe src={url} frameborder="0" className="embed-responsive-item"></iframe>
+					<iframe src={url} frameBorder="0" className="embed-responsive-item"></iframe>
 				</div>
 				<div className="details">
 					<div>{video.snippet.title}</div>
