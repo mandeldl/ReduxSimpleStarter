1f63f21fd1da090f76a63ab1799379c0eabbc7d6
David Mandel <mandeldl@gmail.com>
2016-11-30 14:45:55 -0500

    add videodetails to main app

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import YTSearch from 'youtube-api-search';
 //Components:
 import SearchBar from './components/search_bar';  //Note relative paths, .js extension isn't necessary
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 //Special API Key for YouTube, hidden because of Gitignore
 import {API_KEY} from './API_KEY';  
@@ -26,6 +27,7 @@ class App extends Component {
 		return (
 			<div>
 				<SearchBar />
+				<VideoDetail video={this.state.videos[0]}/>
 				<VideoList videos={this.state.videos} />
 			</div>
 			)
